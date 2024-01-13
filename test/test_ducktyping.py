9d636db34145b98dd480ced2c536a55549ba842e
yudaitnb <yudai.tnb@gmail.com>
2024-01-13 22:52:03 +0900

    add unittest by pytest

---
+++ test/test_ducktyping.py
@@ -0,0 +1,15 @@
+from src.compiler import Compiler
+from src.syntax.semantic_object import *
+
+def test():
+    # テスト用のソースコードを読み込む
+    with open("test/sample/ducktyping.py", "r") as f:
+        source_code = f.read()
+
+    # コンパイラのインスタンスを作成
+    result = Compiler(source_code).get_result_fullpath()
+
+    # 結果を検証
+    assert isinstance(result, VObject)
+    assert result.type_tag == "Swim"
+    assert result.attributes == {}
---
+++ test/test_latebinding.py
@@ -0,0 +1,16 @@
+from src.compiler import Compiler
+from src.syntax.semantic_object import *
+
+def test():
+    # テスト用のソースコードを読み込む
+    with open("test/sample/latebinding.py", "r") as f:
+        source_code = f.read()
+
+    # コンパイラのインスタンスを作成
+    result = Compiler(source_code).get_result_fullpath()
+
+    # 結果を検証
+    assert isinstance(result, VObject)
+    assert result.type_tag == "Container"
+    next = result.attributes["item"]
+    assert next.type_tag == "Node"
