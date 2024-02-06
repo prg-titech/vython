ad72c641bbf08b43d5162cb3bc9bd042fd3893c8
Satsuki Kasuya <satsuki942@gmail.com>
2024-02-06 18:25:44 +0900

    modifying error message

--- src/compatibilitychecker.py
+++ src/compatibilitychecker.py
@@ -7,5 +7,5 @@ def checkCompatibility(vt1, vt2):
     if vt1.compat(vt2) == True:
         pass
     else:
-        raise TypeError(f"互換性のない値を混ぜて使っている: クラス{x[0]}のバージョン{x[1]},{x[2]}の値")
+        raise TypeError(f"Inconsistent Version Usage:\nComparing {x[0]}!{x[1]} and {x[0]}!{x[2]} values")
         
